0c984db3f607f8367d864cb8571abc8b0132d2bc
Dunklas <dunklas@gmail.com>
2020-01-29 22:58:55 +0100

    Add endpoint /groups/{id} (#2)
    * Add another endpoint to get a single group

--- src/main/java/com/github/tradfrihttp/controller/GetGroup.java
+++ src/main/java/com/github/tradfrihttp/controller/GetGroup.java
@@ -4,21 +4,20 @@ import com.github.tradfrihttp.model.LightGroup;
 import com.github.tradfrihttp.tradfricoaps.TradfriCoapsApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-public class GetGroups {
+public class GetGroup {
 
     @Autowired
     private TradfriCoapsApi coapApi;
 
-    @GetMapping("/groups")
+    @GetMapping("/groups/{id}")
     @ResponseBody
-    public List<LightGroup> groups() {
-        return coapApi.getGroups();
+    public LightGroup group(@PathVariable Integer id) {
+        return coapApi.getGroup(id);
     }
 
 }
