b9963cc3b34c73503b892d4f1c251e40fbed8d09
Dunklas <dunklas@gmail.com>
2020-02-01 11:33:24 +0100

    Add handling of COAPS response codes (#6)

--- src/test/java/com/github/tradfrihttp/tradfricoaps/LightsHandlerTest.java
+++ src/test/java/com/github/tradfrihttp/tradfricoaps/LightsHandlerTest.java
@@ -30,7 +30,7 @@ public class LightsHandlerTest {
 
     @Test
     void getLightShouldHandleValidResponse() throws InterruptedException, TradfriCoapsApiException {
-        Response mockResponse = new Response(CoAP.ResponseCode.VALID);
+        Response mockResponse = new Response(CoAP.ResponseCode.CONTENT);
         mockResponse.setPayload("{\"9001\":\"TRADFRI bulb\",\"9002\":1579467404,\"9020\":1580453223,\"9019\":1,\"9003\":65537,\"9054\":0,\"5750\":2,\"3\":{\"1\":\"TRADFRI bulb E14 WS opal 600lm\",\"0\":\"IKEA of Sweden\",\"2\":\"\",\"3\":\"2.0.023\",\"6\":1},\"3311\":[{\"5850\":1,\"9003\":0,\"5851\":137,\"5717\":0,\"5711\":370,\"5709\":32886,\"5710\":27217,\"5706\":\"f1e0b5\"}]}");
         when(client.get(any())).thenReturn(mockResponse);
         LightBulb lightBulb = lightsHandler.handleGetLight(0);
@@ -43,6 +43,28 @@ public class LightsHandlerTest {
         assertEquals(32886, lightBulb.xColor);
         assertEquals(27217, lightBulb.yColor);
         assertEquals("f1e0b5", lightBulb.hexColor);
+    }
+
+    @Test
+    void getLightShouldHandleNotFound() throws InterruptedException {
+        Response mockResponse = new Response(CoAP.ResponseCode.NOT_FOUND);
+        when(client.get(any())).thenReturn(mockResponse);
+        try {
+            lightsHandler.handleGetLight(0);
+        } catch (TradfriCoapsApiException tcai) {
+            assertEquals(HttpStatus.NOT_FOUND, tcai.getHttpStatus());
+        }
+    }
+
+    @Test
+    void getLightShouldHandleUnexpectedStatusCode() throws InterruptedException {
+        Response mockResponse = new Response(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
+        when(client.get(any())).thenReturn(mockResponse);
+        try {
+            lightsHandler.handleGetLight(0);
+        } catch (TradfriCoapsApiException tcai) {
+            assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, tcai.getHttpStatus());
+        }
     }
 
     @Test
